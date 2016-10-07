b589837ecdc67a035ee46d9c8c63e78a19f2f526
Oscar Larriega - Personal Macbook Pro <larriega@gmail.com>
2016-10-06 21:41:41 -0500

    Add corousel to icons layers

--- js/custom.js
+++ js/custom.js
@@ -1,66 +1,65 @@
 $(function() {
 
     "use strict";
-	
+
 	// REMOVE # FROM URL
 	$( 'a[href="#"]' ).click( function(e) {
 		e.preventDefault();
 	});
-	
+
 	// Tool Tip
 	$('[data-toggle="tooltip"]').tooltip();
 
-	
-	// Menu Card Carousel
-	$("#menu-card-carousel").owlCarousel({
-		autoPlay: 6000, //Set AutoPlay to 3 seconds
-		singleItem : true,
-		transitionStyle : "fade", 
-		stopOnHover : true,
-		navigation : true, // Show next and prev buttons
-		navigationText : ["<i class=\"fa fa-chevron-left animation\"></i>","<i class=\"fa fa-chevron-right animation\"></i>"],
-		pagination : false
-	});
-	
 	// Special Offers Carousel
-	$("#special-offers-carousel").owlCarousel({
-		autoPlay: 6000, //Set AutoPlay to 3 seconds
-		items : 4,
-		stopOnHover : true,
-		navigation : true, // Show next and prev buttons
-		navigationText : ["<i class=\"fa fa-chevron-left animation\"></i>","<i class=\"fa fa-chevron-right animation\"></i>"],
-		pagination : false
-	});	
-	
+	var $carouselEspecial = $("#columna_premios");
+	if ($carouselEspecial.length) {
+		$carouselEspecial.owlCarousel({
+			navigation : false,
+			autoPlay: 3000, //Set AutoPlay to 3 seconds
+			slideSpeed: 300,
+			paginationSpeed : 400,
+			singleItem : true
+		});
+	}
+
 	//MAGNIFIC POPUP
-	$(".gallery-grid").magnificPopup({
-		delegate: 'a.zoom', 
-		type: 'image',
-		gallery: {
-			enabled: true
-		}
-	});
-	
+	var $galleryGrid = $(".gallery-grid");
+	if ($galleryGrid.length) {
+		$galleryGrid.magnificPopup({
+			delegate: 'a.zoom',
+			type: 'image',
+			gallery: {
+				enabled: true
+			}
+		});
+	}
+
 	// Date Picker
-	$(".datepickerInput").datepicker({
-		format: "dd/mm/yyyy"
-	});
-	
+	var $datepicker = $(".datepickerInput");
+	if ($datepicker.length) {
+		$datepicker.datepicker({
+			format: "dd/mm/yyyy"
+		});
+	}
+
 	// Tabs Google Map Init
 	$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
 		initialize();
 	});
-	
+
 	// GOOGLE MAP
-	$(".contact-page-map").height(440);
-	function initialize($) {
-		var mapOptions = {	
-			zoom: 8,
-			center: new google.maps.LatLng(17.421306, 78.457553),
-			disableDefaultUI: true
-		};
-		var map = new google.maps.Map(document.querySelector('.map, .contact-page-map'), mapOptions);
+	var $google = $(".contact-page-map");
+	if ($google.length) {
+		$google.height(440);
+		function initialize($) {
+			var mapOptions = {
+				zoom: 8,
+				center: new google.maps.LatLng(17.421306, 78.457553),
+				disableDefaultUI: true
+			};
+			var map = new google.maps.Map(document.querySelector('.map, .contact-page-map'), mapOptions);
+		}
+		google.maps.event.addDomListener(window, 'load', initialize);
 	}
-	google.maps.event.addDomListener(window, 'load', initialize);
-	
+
 });
