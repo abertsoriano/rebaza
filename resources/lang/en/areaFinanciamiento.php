0e5d96ad2de1cc29d407495b07122b1647a6564d
Oscar Larriega <larriega@gmail.com>
2017-03-04 21:29:37 -0500

    Add translate areaFinanciamiento

---
+++ resources/lang/en/areaFinanciamiento.php
@@ -0,0 +1,14 @@
+<?php
+return [
+	'title' => 'Financings',
+	'list' => '<li>Project Finance</li>
+			<li>Syndicated loans</li>
+			<li>Bank loans</li>
+			<li>Multilateral financing</li>
+			<li>Refinancing</li>
+			<li>Financing through securities offerings (corporate bond issuances and securitizations)</li>',
+
+	'text' => '<p>Working closely with banks and multilateral organizations involved in financing either as lenders or structuring agents, we have developed several legal arrangements for conventional credit and bank transactions, as well as highly complex syndicated loans, including A/B loans, subordinated credits, contributions through preferential stock featuring pegged dividends or yields, and financings in the capitals market through corporate bond issuances, and project and securitization bonds (see section on the stock market above).</p>
+		<p>Our attorneys’ experiences is not restricted to managing the financial side of these transactions. Rather, it also includes regulatory considerations in the electricity, gas and oil, mining, water and sewage, telecommunications, and foreign investment sectors (including Bilateral Investment Treaties or BITs and eventual conflicts to be resolved in agreed arbitration forums such as the Chamber of Commerce, AMCHAM, ICSID, and others).</p>'
+
+];
---
+++ resources/lang/es/areaFinanciamiento.php
@@ -0,0 +1,14 @@
+<?php
+return [
+	'title' => 'Financiamientos',
+	'list' => '<li>Project Finance</li>
+				<li>Créditos Sindicados</li>
+				<li>Crédito Bancarios</li>
+				<li>Créditos con Organismos Multilaterales</li>
+				<li>Refinanciamientos y Reestructuración</li>
+				<li>Financiamiento mediante ofertas públicas de valores (ver emisiones de bonos corporativos y titulizaciones).</li>',
+
+	'text' => '<p>En colaboración muy cercana con los bancos y organismos multilaterales que participan en el financiamiento sea como prestamistas o estructuradores, desarrollamos diversos esquemas legales tanto en operaciones crediticias convencionales, bancarias; como sindicadas de gran complejidad, incluyendo: A/B loans, créditos subordinados, aporte vía acciones preferentes con dividendos o rendimientos indexados; o en financiamientos a través del mercado de capitales vía: bonos corporativos, bonos de proyecto o bonos de titulización.</p>
+		<p>La experiencia de los abogados de Rebaza, Alcázar & De Las Casas no se limita al manejo de los aspectos financieros, sino que también incluye los aspectos regulatorios correspondientes incluyendo sector eléctrico, gas e hidrocarburos, minería, agua y desagüe, telecomunicaciones, inversión extranjera (incluyendo Bilateral Investment Treaties (BIT)), administrativos y de eventuales discrepancias a ser ventiladas en el fuero arbitral acordado (Cámara de Comercio, AMCHAM, ICSID, entre otros).</p>'
+
+];
