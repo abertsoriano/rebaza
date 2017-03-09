060b179ebdb03f2bda9acc0f07217e3f8b4ec9d6
Oscar Larriega <larriega@gmail.com>
2017-03-08 23:10:27 -0500

    Change url admin layout, add image not picture in list articles on home page

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -11,12 +11,18 @@
                     @foreach($articles as $article)
                     <div class="row">
                         <div class="col-sm-3">
+                            @if ($article->imagen != '')
                             <img src="{{ asset('images/articles/' . $article->imagen) }}" alt="{{ $article->imagen }}" class="img-responsive">
+                            @else
+                                <img src="{{ asset('images/not-pictured.jpg') }}" alt="Not picture" class="img-responsive">
+                            @endif
                         </div>
                         <div class="col-sm-9">
                             <div class="row">
                                 <div class="col-sm-12">
+                                    @if ($article->title != '')
                                     <h3>{{ $article->title }}</h3>
+                                    @endif
                                 </div>
                                 <div class="col-sm-12">
                                     {!! $article->desc !!}
