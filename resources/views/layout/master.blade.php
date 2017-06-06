9ad8272419943c5b7e6635fd16fbf6334041823c
Oscar Larriega <larriega@gmail.com>
2017-06-06 12:28:29 -0500

    Prevent load cache css

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -18,7 +18,7 @@ if ($locale === 'en') {
 	<meta name="author" content="">
 
 	<title>Rebaza, Alcazar @yield('title', '')</title>
-	<link href="{{ asset('css/main.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/main.css') }}?c={{ time() }}" rel="stylesheet">
 	<!--[if lt IE 9]>
 	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
